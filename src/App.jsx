62be5f1d72e03ec93f74de1ee9edde03ad6c4195
Kaodily <isizohchika@gmail.com>
2023-09-08 20:59:25 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -21,30 +21,34 @@ function App() {
     <div className=" flex justify-center pt-20 md:pt-72 lg:pt-16 xl:pt-44">
       <div className="w-[90%] shadow-md md:w-[50%] lg:w-[40%] bg-white h-max p-6">
         <p data-testid="slackUserName" className="font-bold mb-3">
-          Slack Name: <span className="font-normal"> kaodilim</span>
+           Slack Name: kaodilim
         </p>
-        <div className="flex gap-3 " data-testid="slackDisplayImage">
+        <div className="flex gap-3 ">
           <p className="font-bold"> Slack Display Picture:</p>
-          <img width={200} src={hero} alt="hero image" />
+          <img
+            data-testid="slackDisplayImage"
+            width={200}
+            src={hero}
+            alt="kaodilim"
+          />
         </div>
         <p data-testid="currentDayOfTheWeek" className="font-bold mt-3">
-          Current Day of the Week:
-          <span className="font-normal"> {weekday[currentDay]}</span>
+           Current Day of the Week: { weekday[currentDay]}
         </p>
         <p data-testid="currentUTCTime" className="font-bold mt-3">
-          Current UTC Time:
-          <span className="font-normal">
-            {" "}
-            {`${hours}:${minutes}:${seconds}.${milliseconds}`}
-          </span>
+           Current UTC Time: 
+           { `${ hours}:${minutes}:${seconds}.${milliseconds}`}
         </p>
         <p data-testid="myTrack" className="font-bold mt-3">
-          Track: <span className="font-normal"> Frontend</span>
+          Track: Frontend
         </p>
-        <a data-testid="githubURL" href="https://github.com/Kaodily/stage-one" target="_blank" rel="noreferrer">
-          <button className="w-[100%] p-4 rounded-md mt-3 hover:bg-black hover:text-white border-2 border-black">
+        <a
+        className=" block w-[100%] text-center p-4 rounded-md mt-3 hover:bg-black hover:text-white border-2 border-black"
+          data-testid="githubURL"
+          href="https://github.com/Kaodily/stage-one"
+          target="_blank"
+          rel="noreferrer">
             GitHub URL
-          </button>
         </a>
       </div>
     </div>
